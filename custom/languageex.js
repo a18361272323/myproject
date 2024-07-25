92549cbf6cb5a08260ed768200b4019384f550fc
a18361272323 <57470777+a18361272323@users.noreply.github.com>
2024-07-25 13:40:28 +0800

    1

---
+++ custom/languageex.js
@@ -0,0 +1,161 @@
+var gloginPageBgUrl = 'login_bg.jpg'; // 请修改为登录界面背景文件名
+var gloginLogo = 'login_icon_logo.png'; //请修改为网页logo文件名
+var gmainLogo = 'nav_icon_head.png'; //请修改为网页logo文件名
+var gcustomeexperience = 'demo';
+var gdefaultLanguage = 'zh'; // zh en
+var gdefaultLatLon = { lat: 36.11, lon: 106 }; // map center
+var glogin_input_backgroud_color = '#ffffff';
+var glogin_input_font_color = '#999999';
+var glogin_input_font_select_color = '#000000';
+var glogin_page_font_color = '#ffffff';
+var gShowAppQRCode = true;
+var gShowWxQRCode = true;
+var gShowVersion = true;
+var gShowRegister = false;
+var gShowDemo = true;
+var gShowSelectLanguage = true;
+var gMarkerWidth = 32;
+window.loginPage = 'DefaultLogin'; // 自定义登录页面 DefaultLogin CustomLogin
+
+var customZh = {
+    // login: {
+    //     title: '请修改中文标题',
+    //     shorttitle: '请修改监控界面标题',
+    //     beiAn1: '编辑中文备案1的信息',
+    //     beiAn2: '编辑中文备案2的信息',
+    // }
+    // reportForm: {
+    //     altitude: '海拔',
+    //     altitudeunit: '米',
+    // }
+};
+
+var customEn = {
+    // login: {
+    //     title: 'please edit title in English',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English',
+    //     beiAn2: 'Edit beian2 hint in English',
+    // }
+};
+
+var customFr = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+var customEs = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+
+var customPt = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+var customRu = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+var customId = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+var customTw = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+var customTr = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+// 伊朗 波斯语
+var customFa = {
+    // login: {
+    //     title: 'please edit title in English ',
+    //     shorttitle: 'please edit monitor page title in English',
+    //     beiAn1: 'Edit beian1 hint in English ',
+    //     beiAn2: 'Edit beian2 hint in English ',
+    // }
+};
+
+// 日语
+var customJa = {
+    login: {
+        // title: '日本语言',
+        // shorttitle: 'please edit monitor page title in English',
+        // beiAn1: 'Edit beian1 hint in English ',
+        // beiAn2: 'Edit beian2 hint in English ',
+    }
+};
+
+// 越南语
+var customVi = {
+    login: {
+        // title: '越南语',
+        // shorttitle: 'please edit monitor page title in English',
+        // beiAn1: 'Edit beian1 hint in English ',
+        // beiAn2: 'Edit beian2 hint in English ',
+    }
+};
+
+// 阿拉伯
+var customAr = {
+    login: {
+        // title: '越南语',
+        // shorttitle: 'please edit monitor page title in English',
+        // beiAn1: 'Edit beian1 hint in English ',
+        // beiAn2: 'Edit beian2 hint in English ',
+    }
+};
+
+window.gloginPageBgUrl = gloginPageBgUrl;
+window.gloginLogo = gloginLogo;
+window.gmainLogo = gmainLogo;
+window.gcustomeexperience = gcustomeexperience;
+window.gdefaultLatLon = gdefaultLatLon;
+window.gdefaultLanguage = gdefaultLanguage;
+window.gShowAppQRCode = gShowAppQRCode;
+window.glogin_input_backgroud_color = glogin_input_backgroud_color;
+window.glogin_input_font_select_color = glogin_input_font_select_color;
+window.glogin_input_font_color = glogin_input_font_color;
+window.glogin_page_font_color = glogin_page_font_color;
+window.gShowVersion = gShowVersion;
+window.gShowDemo = gShowDemo;
+window.gShowSelectLanguage = gShowSelectLanguage;
+window.gMarkerWidth = gMarkerWidth;
+window.gMarkerWindowDy = -(gMarkerWidth / 2);
